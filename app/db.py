8488903a009d55651e8c445250c047836ddef7af
Onur Mutlu <mutlu_o@hotmail.com>
2025-04-21 23:51:57 +0300

    .env updates

--- app/db.py
+++ app/db.py
@@ -1,9 +1,13 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env
 
 db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="OmOm1989!!",
-    database="shopping_app"
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
 )
 cursor = db.cursor(dictionary=True)
